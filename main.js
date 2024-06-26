aa2c8644b7d3cefc5b973f77ad2c3ba4fafd6fa6
thewhitehathacke <151182675+thewhitehathacke@users.noreply.github.com>
2024-06-26 15:15:16 +0530

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose',gotPoses);
 }
 function draw(){
-    background('#969A97');  
+    background('#A020F0');  
 }
 function modelLoaded(){
     console.log('Posenet Is Initiated');
